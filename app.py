ac20e087498282868bfc763b8c2561985c000585
Kai-Pratt <kai.pratt9@gmail.com>
2025-04-11 08:42:47 +1000

    added database stuff

--- app.py
+++ app.py
@@ -1,7 +1,14 @@
 from flask import Flask, render_template, request, redirect, url_for
-import sqlite3
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///cms.db'
+db = SQLAlchemy(app)
+
+class Content(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    body = db.Column(db.Text, nullable=False)
 
 @app.route('/')
 def home():
@@ -32,4 +39,6 @@ def create():
     return render_template('edit.html')
 
 if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()
     app.run(debug=True)
