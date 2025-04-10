6e30da312ebb0030ec9a3f6329e33138566c40ae
Kai-Pratt <kai.pratt9@gmail.com>
2025-04-11 08:32:20 +1000

    added a bunch of app routes

--- app.py
+++ app.py
@@ -7,5 +7,29 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
+@app.route('/login')
+def login():
+    pass
+
+@app.route('/view')
+def view():
+    pass
+
+@app.route('/register')
+def register():
+    pass
+
+@app.route('/create')
+def create():
+    pass
+
+@app.route('/delete')
+def create():
+    pass
+
+@app.route('/edit')
+def create():
+    pass
+
 if __name__ == '__main__':
     app.run(debug=True)
