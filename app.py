174c20a7f03f6f83093040072ce16e45400d584a
Kai-Pratt <kai.pratt9@gmail.com>
2025-04-11 08:35:52 +1000

    added empty html pages

--- app.py
+++ app.py
@@ -5,31 +5,31 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    return render_template('home.html')
 
 @app.route('/login')
 def login():
-    pass
+    return render_template('login.html')
 
 @app.route('/view')
 def view():
-    pass
+    return render_template('view.html')
 
 @app.route('/register')
 def register():
-    pass
+    return render_template('register.html')
 
 @app.route('/create')
 def create():
-    pass
+    return render_template('create.html')
 
 @app.route('/delete')
 def create():
-    pass
+    return render_template('delete.html')
 
 @app.route('/edit')
 def create():
-    pass
+    return render_template('edit.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
